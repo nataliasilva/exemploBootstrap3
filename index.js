a1ff6905d27c14015f45b693ba59aed0bbc93711
Natalia Silva <nataliasilva.contato@hotmail.com>
2017-07-23 22:28:50 -0300

    commit initial

---
+++ index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+app.use(express.static("./"));
+app.get("/", (req, res) => {
+    res.send(__dirname + "/index.html");
+});
+app.listen(8080, function () {
+    console.log("Servidor rodando na porta 8080");
+});
