dd868c1d36d4ebef479b6380d4472033fcd006a6
Natalia Silva <nataliasilva.contato@hotmail.com>
2017-07-23 23:09:51 -0300

    foram feitas algumas atleracoes no html

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ app.use(express.static("./"));
 app.get("/", (req, res) => {
     res.send(__dirname + "/index.html");
 });
-app.listen(8080, function () {
+app.listen(8081, function () {
     console.log("Servidor rodando na porta 8080");
 });
